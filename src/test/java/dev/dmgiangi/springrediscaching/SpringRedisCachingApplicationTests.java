27939b7277e714ecd093b52fcaee3bd2b55f7f34
Gianluigi De Marco <dmgiangi@gmail.com>
2022-11-08 17:01:54 +0100

    initial commit

---
+++ src/test/java/dev/dmgiangi/springrediscaching/SpringRedisCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dmgiangi.springrediscaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisCachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
